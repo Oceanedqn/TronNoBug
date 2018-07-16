ee64e80ec1254fb161e2a3fb11195cd2394d469e
Oceane <oceane.dqn@gmail.com>
2018-07-16 12:24:50 +0200

    J'ai testé un truc mais bonnnn

---
+++ view/src/test/java/mock/ChronoMock.java
@@ -0,0 +1,145 @@
+/*
+ *  Océane Duquenne
+ * Join : oceane.duquenne@viacesi.fr
+ * 
+ * Version 1.0
+ */
+package mock;
+
+import controller.IChrono;
+
+/**
+ * The Class Chrono.
+ */
+public class ChronoMock implements Runnable {
+
+	/** The chrono. */
+	private IChrono chrono;
+	
+	/** The pause. */
+	private final int PAUSE = 1000;
+	
+	/** The compteur temps. */
+	private int compteurTemps;
+	
+	/** The str. */
+	private String str;
+	
+	/** The final time. */
+	private int finalTime = 50;
+	
+	/** The is end. */
+	private boolean isEnd = true;
+
+	/** The count. */
+	Thread count = new Thread(this);
+
+	/**
+	 * Instantiates a new chrono.
+	 */
+	public ChronoMock() {
+		this.compteurTemps = 45;
+		this.str = "45";
+
+
+		count.start();
+	}
+
+
+
+
+	/*
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+
+
+		while (isEnd) {
+			try {
+				Thread.sleep(PAUSE);
+			} catch (InterruptedException e) {
+			}
+			this.compteurTemps++;
+			this.str = "" + this.compteurTemps;
+			// System.out.println(compteurTemps + " methode run");
+		}
+
+	}
+
+	/**
+	 * Stop chrono.
+	 */
+	public void stopChrono() {
+		isEnd = false;
+	}
+
+
+
+	/**
+	 * Gets the compteurs temps.
+	 *
+	 * @return the compteurs temps
+	 */
+	public int getCompteursTemps() {
+
+		return compteurTemps;
+	}
+
+	/**
+	 * Gets the str.
+	 *
+	 * @return the str
+	 */
+	public String getStr() {
+		// System.out.println(str + "getstr");
+		return str;
+	}
+
+
+
+	/**
+	 * Checks if is end.
+	 *
+	 * @return true, if is end
+	 */
+	public boolean isEnd() {
+		return isEnd;
+	}
+
+	/**
+	 * Gets the final time.
+	 *
+	 * @return the final time
+	 */
+	public int getFinalTime() {
+		return finalTime;
+	}
+
+	/**
+	 * Sets the final time.
+	 *
+	 * @param finalTime the new final time
+	 */
+	public void setFinalTime(int finalTime) {
+		this.finalTime = finalTime;
+	}
+
+	/**
+	 * Gets the chrono.
+	 *
+	 * @return the chrono
+	 */
+	public IChrono getChrono() {
+		return chrono;
+	}
+
+	/**
+	 * Sets the chrono.
+	 *
+	 * @param chrono the new chrono
+	 */
+	public void setChrono(IChrono chrono) {
+		this.chrono = chrono;
+	}
+
+}
